7a647e22fe43a2f8ad52f3bd4a0b541f600fc6ac
Maxi-Dominguez <maximiliano.dominguez@pyxis.com.uy>
2018-01-26 13:09:04 -0300

    CUP-1 Segunda versión

---
+++ js/javascript.js
@@ -0,0 +1,7 @@
+function enviar(){
+  window.location.href = "/home/maxi/Escritorio/Proyecto Notebook Maxi Anima/Proyecto_clinica/HistoriaClinicaPersonal/datos-familiares.html";
+}
+
+function volver(){
+  window.location.href = "/home/maxi/Escritorio/Proyecto Notebook Maxi Anima/Proyecto_clinica/HistoriaClinicaPersonal/carnet_home.html";
+}
